2b925c5d2047141f6867a49ca3de7d810132fdf9
crazymakcter <crazymakcter@gmail.com>
2014-07-27 20:24:18 +0300

    webdriver

--- src/testcases/UploadPicTest.java
+++ src/testcases/UploadPicTest.java
@@ -1,10 +1,19 @@
 package testcases;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 import org.testng.AssertJUnit;
 import org.testng.Assert;
 import org.testng.SkipException;
 import org.testng.annotations.Test;
+
+import com.gargoylesoftware.htmlunit.javascript.configuration.WebBrowser;
 
 public class UploadPicTest {
 	@Test
@@ -13,7 +22,7 @@ public class UploadPicTest {
 		//skipping a test
 		throw new SkipException ("Skipping test - any reason");
 		//webdrivercode
-		
+
 		//expected and actual title
 		String expected="A";
 		String actual="A";
@@ -21,7 +30,7 @@ public class UploadPicTest {
 		//Assertion in testing
 		AssertJUnit.assertEquals(actual, expected);
 		//fail - condition - false
-		AssertJUnit.assertTrue(3>12, "Some error msg");
+		AssertJUnit.assertTrue(3>2, "Some error msg");
 		
 	}
 	}
